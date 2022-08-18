5812541c306f25970754fbbfb890cb16aa2cc946
Yaël Nseke-Nzinda <www.yael.nseke@gmail.com>
2022-08-18 10:19:01 +0200

    slider fonctionnem

--- scripts/script-slider.js
+++ scripts/script-slider.js
@@ -1,29 +1,27 @@
-const scrollRight = document.querySelector(".scrollRight")
-const element = document.querySelector(".container")
+const scrollRight = document.querySelector(".scrollRight");
+const element = document.querySelector(".container");
 
-scrollRight.addEventListener("click", function (){
-    element.scrollLeft+=500;
+scrollRight.addEventListener("click", function () {
+  element.scrollLeft += 500;
 });
 
+const scrollLeft = document.querySelector(".scrollLeft");
 
-const scrollLeft = document.querySelector(".scrollLeft")
+scrollLeft.addEventListener("click", function () {
+  element.scrollLeft -= 500;
+});
 
-scrollLeft.addEventListener("click", function() {
-    element.scrollLeft-=500;
-})
+var container = document.getElementById("sliderImg");
+var imgs = container.getElementsByTagName("img");
+var apercu = document.getElementById("apercu");
+var counter = 0;
 
+for (let i = 0; i < imgs.length; i++) {
+  let img = imgs[i];
 
-var container = document.getElementById("container")
-var imgs = container.getElementsByTagName("img")
-var apercu = document.getElementById("apercu")
-var counter=0;
-
-for(let i=0;i<imgs.length;i++){
-let img=imgs[i]
-
-
-img.addEventListener("click",function(){
-apercu.src=this.src
-})
-
+  img.addEventListener("click", function () {
+    console.log(this);
+    console.log(this.src);
+    apercu.src = this.src;
+  });
 }
